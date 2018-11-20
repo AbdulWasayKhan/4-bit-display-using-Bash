f582227eaab7b4d141fbf385d7158902a2a7ade8
AbdulWasayKhan <awkhan94@my.yorku.ca>
2018-11-20 02:22:39 +0000

    Final Changes

--- setbits.sh
+++ setbits.sh
@@ -8,24 +8,24 @@ done
 
 	if (($1 & 1))
 	then 
-		gpio write 1 1	
+		gpio write 3 1	
 	else
-		gpio write 1 0
+		gpio write 3 0
 	fi
 
 	if (($1 >> 1 &1))
 	then
 	
-        	gpio write 3 1
+        	gpio write 2 1
 	else  
-		gpio write 3 0
+		gpio write 2 0
 	fi
 
 	if (($1 >> 2 &1))
 	then
-        	gpio write 2 1  
+        	gpio write 1 1  
 	else
-		gpio write 2 0
+		gpio write 1 0
 	fi
 	
 	if (($1 >> 3 & 1))
